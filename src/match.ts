b6909707114590c8bdd517198e92b14c464aab51
Alex Demskie <alex.demskie@gmail.com>
2019-07-16 21:03:13 -0700

    make lodash a non-dev dependency

--- src/match.ts
+++ src/match.ts
@@ -2,6 +2,15 @@ import * as shared from "./shared";
 import * as sort from "./sort";
 import { Network } from "./network";
 
+/**
+ * Create a collection of networks to query for matches
+ *
+ * @example
+ * var matcher = new netparser.Matcher(["192.168.0.0/24", "192.168.4.0/23"]);
+ * matcher.has("192.168.5.0"); // returns true
+ *
+ * @returns an object for adding and querying for network matches
+ */
 export class Matcher {
   private readonly sorted = [] as Network[];
 
@@ -17,6 +26,18 @@ export class Matcher {
     this.sorted = shared.summarizeSortedNetworks(subnets);
   }
 
+  /**
+   * Query for the given network
+   *
+   * @example
+   * var matcher = new netparser.Matcher();
+   * matcher.add("10.0.0.0/24");
+   * matcher.has("192.168.5.0"); // returns false
+   *
+   * @param network - An address or subnet
+   *
+   * @returns A boolean
+   */
   public has(network: string) {
     var net = shared.parseBaseNetwork(network, false, false);
     if (!net || !net.isValid()) return false;
@@ -27,11 +48,24 @@ export class Matcher {
     return false;
   }
 
+  /**
+   * Insert the given network into the Matcher
+   *
+   * @example
+   * var matcher = new netparser.Matcher();
+   * matcher.add("10.0.0.0/24");
+   * matcher.add("255.255.255.255");
+   *
+   * @param network - An address or subnet
+   *
+   * @returns the Matcher object for chaining purposes
+   */
   public add(network: string) {
     var net = shared.parseBaseNetwork(network, false, false);
-    if (!net || !net.isValid()) return;
+    if (!net || !net.isValid()) return this;
     var idx = sort.binarySearchForInsertionIndex(net, this.sorted);
-    if (idx < this.sorted.length && this.sorted[idx].compare(net) === 0) return;
+    if (idx < this.sorted.length && this.sorted[idx].compare(net) === 0) return this;
     this.sorted.splice(idx, 0, net);
+    return this;
   }
 }
